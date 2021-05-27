c45c464fe59bd400d820964e0ba89cc1507c5d76
kushp19 <84835776+kushp19@users.noreply.github.com>
2021-05-27 09:24:17 +0530

    Create demo.php

---
+++ day2/demo.php
@@ -0,0 +1,12 @@
+<?php
+echo "welcomm to php";
+echo "<h1>I am H1 in PHP</h1>";
+?>
+<html> 
+    <body>
+        welcom to HTML code
+        <?php
+        echo "Ian PHP Inside HTML";
+        ?>
+    </body>
+</html>
