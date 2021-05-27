2d8b92bf63643a4b1c6ee9422261c1a975dbc98b
kushp19 <84835776+kushp19@users.noreply.github.com>
2021-05-27 21:47:32 +0530

    Add files via upload

---
+++ Day1/table.php
@@ -0,0 +1,58 @@
+<html>
+
+
+    <body> 
+        <table border="2" cellspacing= "4" cellpadding= "5" width="60%">
+            <caption> TIME TABLE </caption>
+            <tr>
+                <th> TIME </th>
+                <th> monday </th>
+                <th> tuesday </th>
+            </tr> 
+            <tr>
+                <td> 1.20 to 2.10 </td>
+                <td> toc </td>
+                <td> aj </td>
+            </tr>
+            <tr>
+                <td> 2.10 to 3.00 </td>
+                <td> wp </td>
+                <td> mpi </td>
+            </tr>
+            <tr>
+                <td> 3.00 to 3.45 </td>
+                <td rowspan="2"> 1.aj <br>2. iot <br>3.toc </td>
+                <td> toc </td>
+            </tr>
+            <tr>
+                <td> 3.45 to 4.30 </td>
+                <td> wp/dm </td>
+            </tr>
+        </table>
+    </body>
+</html>
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
