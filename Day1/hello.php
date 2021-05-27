4604db3b97fb3122404c0e3983a6833defbdb263
kushp19 <84835776+kushp19@users.noreply.github.com>
2021-05-27 21:44:17 +0530

    Create hello.php

---
+++ Day1/hello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <h1> My First Program With PHP</h1>
+    </head>
+    <body>  
+    
+        <?php
+             echo " <b> hello My name is Kush </b>"  ;
+        ?>
+    </body>
+</html>
