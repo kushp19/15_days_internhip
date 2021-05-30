7fa4618028ffc3769eb4642cfaf91e9af7e7264c
kushp19 <84835776+kushp19@users.noreply.github.com>
2021-05-30 12:59:08 +0530

    Add files via upload

---
+++ Day4/contact.php
@@ -0,0 +1,50 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Online Auction </title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+include './themepart/menu.php';
+include './themepart/logo.php';
+?>
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="Auctionphoto.jpg" width="920" height="420" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Contact</a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                    <p>Mobile Number=999XXXXX12</p>
+                                    <p>Email Address=abc@mail.com</p>
+                                    
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+                <?php 
+                include './themepart/sidebar.php';
+                ?>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+                include './themepart/footer.php';
+?>
+</body>
+</html>
+
+
