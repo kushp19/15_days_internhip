4987759ec9886639c0866493ef2a98941815d623
kushp19 <84835776+kushp19@users.noreply.github.com>
2021-06-01 21:43:35 +0530

    Add files via upload

---
+++ Day6/withtheme/themepart/sidebar.php
@@ -0,0 +1,22 @@
+<div id="sidebar">
+			<ul>
+				<li>
+					<h2>Categories</h2>
+					<ul>
+						<li><a href="#">Antiqe Product</a></li>
+						<li><a href="#">Cheap Product</a></li>
+						<li><a href="#">Costly Product</a></li>
+						<li><a href="#">Rare Product</a></li>
+						
+					</ul>
+				</li>
+				<li>
+					<h2>About Us</h2>
+					<ul>
+						<li><a href="#">Histroy</a></li>
+						<li><a href="#">Best Sold Product</a></li>
+						
+					</ul>
+				</li>
+			</ul>
+		</div>
